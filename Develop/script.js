7533392e1ea6cdbeb8b24bed31ef3231788f0985
alexfortin <fortin.alex14@gmail.com>
2022-12-21 14:12:03 -0600

    created a function to handle all the form data

--- Develop/script.js
+++ Develop/script.js
@@ -11,12 +11,29 @@ const closeModal = () => {
   card.classList.remove("hide")
 }
 
-// Write password to the #password input
-function writePassword() {
+const handleForm = event => {
+  event.preventDefault()
+  let form = document.getElementsByClassName("passForm")
+  const passLength = form[0][0].value
+  const uppercase = form[0][1].checked
+  const lowercase = form[0][2].checked
+  const numbers = form[0][3].checked
+  const specialCharacters = form[0][4].checked
+
+  if(passLength < 8 || passLength > 128) {
+    alert("Password Length must be between 8 - 128 characters")
+  }
+
+  if(uppercase, lowercase, numbers, specialCharacters === false){
+    alert("Please check one of the checkboxes to continue")
+  }
+
+}
+
+const writePassword = formData => {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
